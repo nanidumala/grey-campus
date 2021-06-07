09b1115436875c3a15daf9c52aef8287186cfdc2
root <root@Harshas.localdomain>
2021-06-07 17:32:38 +0530

    added a button in cart view which will connect to javascript cart controller and shows the products in cart as an alert message on clicking the button

--- app/javascript/controllers/cart_controller.js
+++ app/javascript/controllers/cart_controller.js
@@ -3,7 +3,14 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = [ "output" ]
 
-  summary(){
-      alert(this.outputTarget.textContent)
+  Summary(){
+    var output=this.outputTargets
+    var each_one =""
+    for (var i=0,len=output.length;i<len;i++)
+    {
+      each_one +=output[i].textContent;
+
+    }
+      alert(each_one)
   }
 }
